924acf6068397e8a011fd68d3dd0ef5f8bc5f9b7
uotaneet <uotaneet@gmail.com>
2019-03-26 04:37:10 +0000

    first commit after fork

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from mydsl import dsl_core, dsl_mongo, dsl_server
 import yaml
+import os
 
 print(dsl_mongo.loadDslFunctions)
 dsl_mongo.loadDslFunctions(dsl_core.dslFunctions)
@@ -7,4 +8,4 @@ dsl_server.loadDslFunctions(dsl_core.dslFunctions, dsl_core.dslAvailableFunction
 
 with open('router.yml') as yaml_file:
   dsl = yaml.load(yaml_file)
-  router, err = dsl_core.Argument(dsl).evaluate({})
+  router, err = dsl_core.Argument(dsl).evaluate({"port": int(os.environ.get('PORT', 0))})
